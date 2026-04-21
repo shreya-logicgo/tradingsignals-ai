b3ca624c573d90361685de5985ea7466f53a0733
manavatarsariya1 <manavtarsariya@logicgoinfotech.in>
2026-04-21 13:02:26 +0530

    fix: fix the image problem in blogimage component

--- landing-page/next.config.ts
+++ landing-page/next.config.ts
@@ -2,7 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "oaidalleapiprodscus.blob.core.windows.net",
+      },
+      {
+        protocol: "https",
+        hostname: "oaidalleapiprod.blob.core.windows.net",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
